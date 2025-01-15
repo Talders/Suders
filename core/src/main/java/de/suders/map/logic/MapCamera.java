0dd6abc0da641ec4db839fdff21199e3b39143c4
Talders <marioangelopohlmann@gmail.com>
2025-01-15 04:39:20 +0100

    Render fix (map was grided)

--- core/src/main/java/de/suders/map/logic/MapCamera.java
+++ core/src/main/java/de/suders/map/logic/MapCamera.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 public class MapCamera extends OrthographicCamera {
 
     public MapCamera(float spawnX, float spawnY) {
-        this.setToOrtho(false, (Gdx.graphics.getWidth() / 10), Gdx.graphics.getHeight() / 10);
+        this.setToOrtho(false, (Gdx.graphics.getWidth() / 8), Gdx.graphics.getHeight() / 8);
         this.position.x = spawnX; //14
         this.position.y = spawnY; //28
-        this.zoom = 0.1f;
+        this.zoom = 0.0955f;
         this.update();
     }
 }
